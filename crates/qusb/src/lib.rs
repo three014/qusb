5c32629e6d78b56291a287562b7ee270bb2ddabc
Aaron Perez <aap7640@gmail.com>
2024-11-27 22:49:54 -0600

    Wrote some structs and functions to send/recv qusb requests

--- crates/qusb/src/lib.rs
+++ crates/qusb/src/lib.rs
@@ -1,24 +1,210 @@
 use bitflags::bitflags;
-use quinn::{rustls, ClientConfig, Endpoint, ServerConfig};
-use std::{net::SocketAddr, sync::Arc};
+use quinn::{
+    crypto::rustls::{QuicClientConfig, QuicServerConfig},
+    rustls,
+};
+use rand::random;
+use std::{
+    collections::HashMap,
+    hash::BuildHasherDefault,
+    net::{Ipv6Addr, SocketAddr, SocketAddrV6},
+    path::Path,
+    pin::Pin,
+    sync::{Arc, LazyLock, Mutex},
+};
+use tokio::io::{
+    AsyncBufReadExt, AsyncRead, AsyncWrite, AsyncWriteExt, BufReader, BufStream, BufWriter,
+};
+use usb_ids::UsbIds;
+
+mod usb_ids;
+
+static USB_IDS: LazyLock<UsbIds> =
+    LazyLock::new(|| usb_ids::parse(Path::new("./usb-ids")).unwrap());
 
 #[derive(Debug)]
-pub struct Config {
-    server: ServerConfig,
-    client: ClientConfig,
-    bind: SocketAddr,
+struct RWStream {
+    tx: quinn::SendStream,
+    rx: quinn::RecvStream,
+}
+
+impl AsyncRead for RWStream {
+    fn poll_read(
+        mut self: Pin<&mut Self>,
+        cx: &mut std::task::Context<'_>,
+        buf: &mut tokio::io::ReadBuf<'_>,
+    ) -> std::task::Poll<std::io::Result<()>> {
+        Pin::new(&mut self.rx).poll_read(cx, buf)
+    }
+}
+
+impl AsyncWrite for RWStream {
+    fn poll_write(
+        mut self: Pin<&mut Self>,
+        cx: &mut std::task::Context<'_>,
+        buf: &[u8],
+    ) -> std::task::Poll<Result<usize, std::io::Error>> {
+        <quinn::SendStream as AsyncWrite>::poll_write(Pin::new(&mut self.tx), cx, buf)
+    }
+
+    fn poll_flush(
+        mut self: Pin<&mut Self>,
+        cx: &mut std::task::Context<'_>,
+    ) -> std::task::Poll<Result<(), std::io::Error>> {
+        <quinn::SendStream as AsyncWrite>::poll_flush(Pin::new(&mut self.tx), cx)
+    }
+
+    fn poll_shutdown(
+        mut self: Pin<&mut Self>,
+        cx: &mut std::task::Context<'_>,
+    ) -> std::task::Poll<Result<(), std::io::Error>> {
+        <quinn::SendStream as AsyncWrite>::poll_shutdown(Pin::new(&mut self.tx), cx)
+    }
+}
+
+#[derive(Debug)]
+struct ReqLine {
+    tx: Mutex<BufWriter<quinn::SendStream>>,
+    rx: Mutex<BufReader<quinn::RecvStream>>,
+}
+
+#[derive(Debug)]
+struct Session {
+    conn: quinn::Connection,
+    req_line: ReqLine,
+    req_data: Mutex<HashMap<spec::ReqId, spec::Req, nohash_hasher::BuildNoHashHasher<spec::ReqId>>>,
+    unclaimed_unidata_lines: HashMap<
+        spec::PayloadId,
+        BufReader<quinn::RecvStream>,
+        nohash_hasher::BuildNoHashHasher<spec::PayloadId>,
+    >,
+    unclaimed_bidata_lines: HashMap<
+        spec::PayloadId,
+        BufStream<RWStream>,
+        nohash_hasher::BuildNoHashHasher<spec::PayloadId>,
+    >,
+}
+
+impl Session {
+    pub async fn send_req(&self, op: spec::Operation) -> spec::ReqId {
+        let id = spec::ReqId(random());
+        let req = postcard::to_vec_cobs::<_, 32>(&spec::Req { id, op }).unwrap();
+        let mut tx = self.req_line.tx.lock().unwrap();
+        let mut written = 0;
+        while written < req.len() {
+            written += tx.write(&req[written..]).await.unwrap();
+        }
+        tx.flush().await.unwrap();
+
+        id
+    }
+
+    pub async fn recv_req(&self, id: spec::ReqId) -> spec::Req {
+        if let Some(req) = self.req_data.lock().unwrap().remove(&id) {
+            return req;
+        }
+
+        let mut buf = vec![0; 256];
+        loop {
+            buf.clear();
+            let _read = self
+                .req_line
+                .rx
+                .lock()
+                .unwrap()
+                .read_until(0, &mut buf)
+                .await
+                .unwrap();
+
+            let req: spec::Req = postcard::from_bytes_cobs(&mut buf).unwrap();
+            if req.id == id {
+                return req;
+            }
+            self.req_data.lock().unwrap().insert(id, req);
+        }
+    }
+
+    async fn connect(conn: quinn::Connecting) -> Self {
+        let conn = conn.await.unwrap();
+        let (mut tx, rx) = conn.open_bi().await.unwrap();
+        let header = postcard::to_vec_cobs::<_, 32>(&spec::Header {
+            version: spec::VERSION,
+            stream_type: spec::StreamType::Req,
+        })
+        .unwrap();
+        let mut written = 0;
+        while written < header.len() {
+            written += tx.write(&header[written..]).await.unwrap();
+        }
+
+        Self {
+            conn,
+            req_line: ReqLine {
+                tx: Mutex::new(BufWriter::with_capacity(1024, tx)),
+                rx: Mutex::new(BufReader::with_capacity(1024, rx)),
+            },
+            req_data: Mutex::new(HashMap::with_hasher(BuildHasherDefault::default())),
+            unclaimed_unidata_lines: HashMap::with_hasher(BuildHasherDefault::default()),
+            unclaimed_bidata_lines: HashMap::with_hasher(BuildHasherDefault::default()),
+        }
+    }
+
+    async fn accept(conn: quinn::Incoming) -> Self {
+        let conn = conn.await.unwrap();
+        let (tx, rx) = conn.accept_bi().await.unwrap();
+
+        let mut rx = BufReader::with_capacity(1024, rx);
+        let mut buf = vec![0; 128];
+        let _read = rx.read_until(0, &mut buf).await.unwrap();
+        let header: spec::Header = postcard::from_bytes_cobs(&mut buf).unwrap();
+        if header.version != spec::VERSION {
+            panic!("Wrong version!");
+        }
+
+        if header.stream_type != spec::StreamType::Req {
+            panic!("First stream was not a request!");
+        }
+
+        Self {
+            conn,
+            req_line: ReqLine {
+                tx: Mutex::new(BufWriter::with_capacity(1024, tx)),
+                rx: Mutex::new(rx),
+            },
+            req_data: Mutex::new(HashMap::with_hasher(BuildHasherDefault::default())),
+            unclaimed_unidata_lines: HashMap::with_hasher(BuildHasherDefault::default()),
+            unclaimed_bidata_lines: HashMap::with_hasher(BuildHasherDefault::default()),
+        }
+    }
 }
 
 #[derive(Debug, Clone)]
-pub struct QusbPeer {
-    endpoint: Endpoint,
+pub struct Peer {
+    endpoint: quinn::Endpoint,
 }
 
-impl QusbPeer {
-    pub fn bind(config: Config) -> Self {
-        let mut endpoint = Endpoint::server(config.server, config.bind).unwrap();
-        endpoint.set_default_client_config(config.client);
-        Self { endpoint }
+impl Peer {
+    pub fn new(
+        server_tls: rustls::ServerConfig,
+        client_tls: rustls::ClientConfig,
+        bind_addr: Option<SocketAddr>,
+        transport_cfg: quinn::TransportConfig,
+    ) -> Result<Self, Box<dyn std::error::Error>> {
+        let server_tls = quinn::ServerConfig::with_crypto(Arc::new(
+            QuicServerConfig::try_from(server_tls).unwrap(),
+        ));
+        let mut client_tls =
+            quinn::ClientConfig::new(Arc::new(QuicClientConfig::try_from(client_tls).unwrap()));
+        client_tls.transport_config(Arc::new(transport_cfg));
+
+        let mut endpoint = quinn::Endpoint::server(
+            server_tls,
+            bind_addr.unwrap_or(SocketAddrV6::new(Ipv6Addr::UNSPECIFIED, 0, 0, 0).into()),
+        )
+        .unwrap();
+        endpoint.set_default_client_config(client_tls);
+
+        Ok(Peer { endpoint })
     }
 
     pub async fn serve(&self) {
@@ -45,11 +231,63 @@ impl QusbPeer {
     }
 
     pub async fn query_peer(&self, peer_addr: SocketAddr, peer_name: &str) {
-        let conn = self.endpoint.connect(peer_addr, peer_name).unwrap().await.unwrap();
+        let conn = self
+            .endpoint
+            .connect(peer_addr, peer_name)
+            .unwrap()
+            .await
+            .unwrap();
 
-        let (send, recv) = conn.open_bi().await.unwrap();
+        let (mut send, mut recv) = conn.open_bi().await.unwrap();
 
         // Ask the peer for what devices they have to offer
+        // let req = QusbReq {
+        //     version: spec::VERSION,
+        //     req: Req::ListDevices,
+        // };
+
+        // let mut buf = postcard::to_vec::<_, 64>(&req).unwrap();
+        // let mut written = 0;
+        // while written < buf.len() {
+        //     written += send.write(&buf[written..]).await.unwrap();
+        // }
+        // send.finish().unwrap();
+
+        // let mut read = 0;
+        // while let Some(bytes) = recv.read(&mut buf[read..]).await.unwrap() {
+
+        //     read += bytes;
+        // }
+        // buf.truncate(read);
+
+        // let resp = postcard::from_bytes::<QusbResp>(&*buf).unwrap();
+
+        // if resp.version != spec::VERSION {
+        //     panic!("Wrong verison! Them: {}, Us: {}", resp.version, spec::VERSION);
+        // }
+
+        // let payload_id = resp.resp.unwrap();
+
+        // let a = conn.accept_uni().await.unwrap();
+
+        // All streams need to include the Qusb Header:
+        // Version: u16
+        // Id: u64
+        //
+        // QusbPeer will (try to) act as a middleware between
+        // all streams in a connection by storing new connections
+        // in a lost+found.
+        //
+        // Let's imagine a connection with streams A and B. Stream A
+        // gets told by its peer that there's more data in a new stream.
+        // Stream A waits for the new stream, but upon connecting, sees
+        // that it's the wrong stream and it doesn't have the right
+        // data. So, Stream A gives this new stream "C" back to QusbPeer.
+        // Meanwhile, Stream B got told by its peer that there's more
+        // data in a new stream, so it waits for a stream. However,
+        // Stream B can ask QusbPeer for a stream that matches the
+        // data it needs, which in this case was actually Stream C.
+        // QusbPeer then gives Stream C to Stream B.
     }
 }
 
---
+++ crates/qusb/src/usb_ids.rs
@@ -0,0 +1,438 @@
+use core::fmt;
+use std::{
+    collections::HashMap,
+    fs,
+    hash::{BuildHasherDefault, Hash},
+    io,
+    num::ParseIntError,
+    path::Path,
+    str::FromStr,
+};
+
+use nohash_hasher::{BuildNoHashHasher, IsEnabled};
+
+#[derive(Debug, Clone)]
+struct Inner {
+    vendor: HashMap<VendorKey, Box<str>, BuildNoHashHasher<VendorKey>>,
+    product: HashMap<ProductKey, Box<str>, BuildNoHashHasher<ProductKey>>,
+    class: HashMap<ClassKey, Box<str>, BuildNoHashHasher<ClassKey>>,
+    subclass: HashMap<SubclassKey, Box<str>, BuildNoHashHasher<SubclassKey>>,
+    protocol: HashMap<ProtocolKey, Box<str>, BuildNoHashHasher<ProtocolKey>>,
+}
+
+#[derive(Debug, Clone)]
+pub struct UsbIds {
+    inner: Inner,
+}
+
+impl UsbIds {
+    fn parse(buf: &str) -> UsbIds {
+        let mut names = Inner::new();
+        let mut last_state = LastState::Start;
+
+        for (line, _num) in buf.lines().zip(1usize..) {
+            if can_skip(line) {
+                continue;
+            }
+
+            if line.contains("L ") {
+                last_state = LastState::Lang;
+                continue;
+            }
+
+            if let Some((key, text)) = parse_class(line) {
+                if names.class.insert(key, text).is_some() {
+                    // Print message about duplicate vendor spec?
+                }
+                last_state = LastState::Class(key);
+                continue;
+            }
+
+            if let Some((key, text)) = parse_vendor(line) {
+                if names.vendor.insert(key, text).is_some() {
+                    // Etc...
+                }
+                last_state = LastState::Vendor(key);
+                continue;
+            }
+
+            if line.contains("HUT ") {
+                last_state = LastState::Hut;
+                continue;
+            }
+
+            match last_state {
+                LastState::Start | LastState::Lang | LastState::Hut => {}
+                LastState::Class(ClassKey(class)) => {
+                    if let Some((key, text)) = parse_subclass(line, class) {
+                        if names.subclass.insert(key, text).is_some() {
+                            // Err...
+                        }
+                        last_state = LastState::Subclass(key);
+                    }
+                }
+                LastState::Subclass(SubclassKey { class, subclass }) => {
+                    if let Some((key, text)) = parse_subclass(line, class) {
+                        if names.subclass.insert(key, text).is_some() {
+                            // Err...
+                        }
+                        last_state = LastState::Subclass(key);
+                    } else if let Some((key, text)) = parse_protocol(line, class, subclass) {
+                        if names.protocol.insert(key, text).is_some() {
+                            // Err...
+                        }
+                    }
+                }
+                LastState::Vendor(VendorKey(vendor))
+                | LastState::Product(ProductKey { vendor, product: _ }) => {
+                    if let Some((key, text)) = parse_product(line, vendor) {
+                        if names.product.insert(key, text).is_some() {
+                            // Print message about duplicate vendor spec?
+                        }
+                        last_state = LastState::Product(key);
+                    }
+                }
+            }
+        }
+
+        UsbIds { inner: names }
+    }
+
+    pub fn vendor(&self, vendor: u16) -> Option<&str> {
+        self.inner.vendor(vendor)
+    }
+
+    pub fn product(&self, vendor: u16, product: u16) -> Option<&str> {
+        self.inner.product(vendor, product)
+    }
+
+    pub fn class(&self, class: u8) -> Option<&str> {
+        self.inner.class(class)
+    }
+
+    pub fn subclass(&self, class: u8, subclass: u8) -> Option<&str> {
+        self.inner.subclass(class, subclass)
+    }
+
+    pub fn protocol(&self, class: u8, subclass: u8, protocol: u8) -> Option<&str> {
+        self.inner.protocol(class, subclass, protocol)
+    }
+
+    pub fn product_display<'a: 'b, 'b>(&'a self, vendor: u16, product: u16) -> Product<'b> {
+        Product {
+            product_str: self.product(vendor, product),
+            product,
+            vendor_str: self.vendor(vendor),
+            vendor,
+        }
+    }
+
+    pub fn class_display<'a: 'b, 'b>(&'a self, class: u8, subclass: u8, protocol: u8) -> Class<'b> {
+        Class {
+            class_str: self.class(class),
+            class,
+            subclass_str: self.subclass(class, subclass),
+            subclass,
+            protocol_str: self.protocol(class, subclass, protocol),
+            protocol,
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct Class<'a> {
+    class_str: Option<&'a str>,
+    class: u8,
+    subclass_str: Option<&'a str>,
+    subclass: u8,
+    protocol_str: Option<&'a str>,
+    protocol: u8,
+}
+
+impl fmt::Display for Class<'_> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        if self.class == 0 && self.subclass == 0 && self.protocol == 0 {
+            write!(f, "(Defined at Interface level)")?;
+        } else {
+            let class = self.class_str.unwrap_or("unknown class");
+            let subclass = self.subclass_str.unwrap_or("unknown subclass");
+            let protocol = self.protocol_str.unwrap_or("unknown protocol");
+
+            write!(f, "{class} / {subclass} / {protocol} ")?;
+        }
+
+        write!(
+            f,
+            "({:02x}/{:02x}/{:02x})",
+            self.class, self.subclass, self.protocol
+        )
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct Product<'a> {
+    product_str: Option<&'a str>,
+    product: u16,
+    vendor_str: Option<&'a str>,
+    vendor: u16,
+}
+
+impl fmt::Display for Product<'_> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let prod = self.product_str.unwrap_or("unknown product");
+        let vend = self.vendor_str.unwrap_or("unknown vendor");
+
+        write!(
+            f,
+            "{vend} : {prod} ({:04x}:{:04x})",
+            self.vendor, self.product
+        )
+    }
+}
+
+impl Inner {
+    pub fn new() -> Self {
+        Self {
+            vendor: HashMap::with_hasher(BuildHasherDefault::default()),
+            product: HashMap::with_hasher(BuildHasherDefault::default()),
+            class: HashMap::with_hasher(BuildHasherDefault::default()),
+            subclass: HashMap::with_hasher(BuildHasherDefault::default()),
+            protocol: HashMap::with_hasher(BuildHasherDefault::default()),
+        }
+    }
+
+    pub fn vendor(&self, vendor: u16) -> Option<&str> {
+        self.vendor.get(&VendorKey(vendor)).map(Box::as_ref)
+    }
+
+    pub fn product(&self, vendor: u16, product: u16) -> Option<&str> {
+        self.product
+            .get(&ProductKey { vendor, product })
+            .map(Box::as_ref)
+    }
+
+    pub fn class(&self, class: u8) -> Option<&str> {
+        self.class.get(&ClassKey(class)).map(Box::as_ref)
+    }
+
+    pub fn subclass(&self, class: u8, subclass: u8) -> Option<&str> {
+        self.subclass
+            .get(&SubclassKey { class, subclass })
+            .map(Box::as_ref)
+    }
+
+    pub fn protocol(&self, class: u8, subclass: u8, protocol: u8) -> Option<&str> {
+        self.protocol
+            .get(&ProtocolKey {
+                class,
+                subclass,
+                protocol,
+            })
+            .map(Box::as_ref)
+    }
+}
+
+enum LastState {
+    Start,
+    Lang,
+    Class(ClassKey),
+    Subclass(SubclassKey),
+    Vendor(VendorKey),
+    Product(ProductKey),
+    Hut,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+struct VendorKey(u16);
+
+impl IsEnabled for VendorKey {}
+impl Hash for VendorKey {
+    fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
+        state.write_u16(self.0)
+    }
+}
+
+impl FromStr for VendorKey {
+    type Err = ParseIntError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        Ok(VendorKey(u16::from_str_radix(s, 16)?))
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+struct ProductKey {
+    vendor: u16,
+    product: u16,
+}
+
+impl ProductKey {
+    fn from_str_and_vendor(s: &str, vendor: u16) -> Result<Self, ParseIntError> {
+        Ok(ProductKey {
+            vendor,
+            product: u16::from_str_radix(s, 16)?,
+        })
+    }
+}
+
+impl IsEnabled for ProductKey {}
+impl std::hash::Hash for ProductKey {
+    fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
+        let vendor = self.vendor as u32;
+        let product = self.product as u32;
+        let key: u32 = (vendor << 16) | product;
+        state.write_u32(key)
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+struct ClassKey(u8);
+
+impl IsEnabled for ClassKey {}
+impl Hash for ClassKey {
+    fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
+        state.write_u8(self.0)
+    }
+}
+
+impl FromStr for ClassKey {
+    type Err = ParseIntError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        Ok(ClassKey(u8::from_str_radix(s, 16)?))
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+struct SubclassKey {
+    class: u8,
+    subclass: u8,
+}
+impl SubclassKey {
+    fn from_str_and_class(s: &str, class: u8) -> Result<SubclassKey, ParseIntError> {
+        Ok(SubclassKey {
+            class,
+            subclass: u8::from_str_radix(s, 16)?,
+        })
+    }
+}
+
+impl IsEnabled for SubclassKey {}
+impl std::hash::Hash for SubclassKey {
+    fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
+        let class = self.class as u16;
+        let subclass = self.class as u16;
+        let key: u16 = (class << 8) | subclass;
+        state.write_u16(key)
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+struct ProtocolKey {
+    class: u8,
+    subclass: u8,
+    protocol: u8,
+}
+
+impl ProtocolKey {
+    fn from_str_class_and_subclass(
+        s: &str,
+        class: u8,
+        subclass: u8,
+    ) -> Result<ProtocolKey, ParseIntError> {
+        Ok(ProtocolKey {
+            class,
+            subclass,
+            protocol: u8::from_str_radix(s, 16)?,
+        })
+    }
+}
+
+impl IsEnabled for ProtocolKey {}
+impl std::hash::Hash for ProtocolKey {
+    fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
+        let class = self.class as u32;
+        let subclass = self.class as u32;
+        let protocol = self.class as u32;
+        let key: u32 = (class << 16) | (subclass << 8) | protocol;
+        state.write_u32(key)
+    }
+}
+
+fn can_skip(line: &str) -> bool {
+    line.is_empty()
+        || line.starts_with('#')
+        || line.starts_with("PHYSDES ")
+        || line.starts_with("PHY ")
+        || line.starts_with("BIAS ")
+        || line.starts_with("AT ")
+        || line.starts_with("HCC ")
+        || line.starts_with("HID ")
+        || line.starts_with("R ")
+        || line.starts_with("VT")
+}
+
+fn parse_value<F, T>(possible: &str, f: F) -> Option<(T, Box<str>)>
+where
+    F: Fn(&str) -> Result<T, ParseIntError>,
+{
+    possible
+        .split_once(' ')
+        .and_then(|(key_token, rest)| f(key_token).ok().map(|key| (key, Box::from(rest.trim()))))
+}
+
+fn parse_class(line: &str) -> Option<(ClassKey, Box<str>)> {
+    parse_value(line.strip_prefix("C ")?, str::parse::<ClassKey>)
+}
+
+fn parse_product(line: &str, vendor: u16) -> Option<(ProductKey, Box<str>)> {
+    parse_value(line.strip_prefix('\t')?, |token| {
+        ProductKey::from_str_and_vendor(token, vendor)
+    })
+}
+
+fn parse_subclass(line: &str, class: u8) -> Option<(SubclassKey, Box<str>)> {
+    parse_value(line.strip_prefix('\t')?, |token| {
+        SubclassKey::from_str_and_class(token, class)
+    })
+}
+
+fn parse_protocol(line: &str, class: u8, subclass: u8) -> Option<(ProtocolKey, Box<str>)> {
+    parse_value(line.strip_prefix("\t\t")?, |token| {
+        ProtocolKey::from_str_class_and_subclass(token, class, subclass)
+    })
+}
+
+fn parse_vendor(line: &str) -> Option<(VendorKey, Box<str>)> {
+    parse_value(line, str::parse::<VendorKey>)
+}
+
+pub fn parse<P>(path: P) -> io::Result<UsbIds>
+where
+    P: AsRef<Path>,
+{
+    let reader = fs::read_to_string(path)?;
+    Ok(UsbIds::parse(&reader))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn parse_vendor_works() {
+        parse_vendor("0001  Fry's Electronics").unwrap();
+        parse_vendor("0002  Ingram").unwrap();
+        parse_vendor("0003  Club Mac").unwrap();
+    }
+
+    #[test]
+    fn parse_vendor_hex() {
+        parse_vendor("001f  Walmart").unwrap();
+    }
+
+    #[test]
+    fn parse_product_works() {
+        parse_product("\t7778  Counterfeit flash drive [Kingston]", 1).unwrap();
+    }
+}
